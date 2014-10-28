da7d6f9674a01226a047ca5f59fe8d32580503c3
Andrew Sheehan <sheehan.andrew@gmail.com>
2014-10-27 23:08:53 -0500

    updated index

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'sinatra/base'
 class MyApp < Sinatra::Base
 
   get '/' do
-    'continuous deployment with codeship!'
+    'continuous deployment with codeship! now time for a speed test.'
   end
 
 end
