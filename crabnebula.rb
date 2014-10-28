f4f1f6c61522e20085aedc861760c2541ab1f8de
Andrew Sheehan <sheehan.andrew@gmail.com>
2014-10-28 08:47:36 -0500

    added mysql support

--- crabnebula.rb
+++ crabnebula.rb
@@ -1,9 +1,12 @@
 require 'rubygems'
 require 'sinatra/base'
+require 'sinatra/activerecord'
 
 class Crabnebula < Sinatra::Base
 
   get '/' do
+    db_time = database.connection.execute('SELECT CURRENT_TIMESTAMP').first['now']
+    request.logger.info "DB time is #{db_time}"
     erb :hello_world
   end
 
